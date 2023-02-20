b19cd09383dadb02e07559ab469353d7dc4a43e7
rozanfa <rozanfadhilalhafidz@gmail.com>
2023-02-20 15:45:23 +0700

    fix: missing files

---
+++ src/components/Login.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+defineProps<{
+  msg: string;
+}>();
+</script>
+
+<template>
+  <div class="form-container flex flex-column">
+    <h1 class="blue text-center mb-title-form">Login</h1>
+    <div class="flex flex-column">
+      <input
+        class="input-field mb-field-form"
+        v-model="username"
+        placeholder="Username"
+      />
+      <input
+        class="input-field mb-field-form"
+        v-model="password"
+        placeholder="Password"
+      />
+    </div>
+    <p class="text-center mb-24">
+      Belum punya Akun? <a href="/register">Register</a> di sini!
+    </p>
+    <button class="">Login</button>
+  </div>
+</template>
+
+<style scoped>
+.form-container {
+  width: 380px;
+  height: 365px;
+  padding: 32px;
+}
+</style>
