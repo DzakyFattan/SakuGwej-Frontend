08145a7774e71465626e68aaccc4884f03c30b09
Timothy Stanley <timothystanleysetiawan555@gmail.com>
2023-03-05 15:31:42 +0700

    feat: dropdown at filter button

--- src/components/item/rekening/bar/AccountBar.vue
+++ src/components/item/rekening/bar/AccountBar.vue
@@ -7,11 +7,13 @@
 </script>
 
 <template>
-    <div class="flex flex-row rounded-md border-secondary-bocchi w-fit h-20 items-center self-center mx-1 my-2 px-4" >
-        <img src="/src/assets/icons/cart.png" class="w-12 h-12 mr-8">
-        <p class="w-16 mr-[23vh]">{{ account.name }}</p>
-        <p class="w-16 ml-[23vh]">{{ nominal }}</p>
-    </div>
+    <a class="flex flex-row rounded-md border-secondary-bocchi w-fit h-20 items-center self-center mx-1 my-2 px-4" 
+        :class="account.bg_color" 
+        :href="'/rekening/'+account.id+'/detail-rekening/'">
+        <img src="/src/assets/icons/cart-gray.png" alt="cart" class="w-12 h-12 mr-8">
+        <p class="w-16 mr-[23vh] text-color-tertiary-gray">{{ account.name }}</p>
+        <p class="w-16 ml-[23vh] text-color-tertiary-gray">{{ nominal }}</p>
+    </a>
 </template>
 
 <style scoped></style>
